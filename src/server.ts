220c0cd9aaed3f9251cab994a9b5f64c54917aab
Fabio Menozzi <f.menozzi@yahoo.com>
2020-08-18 18:19:11 -0300

    list and create connections done

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './route';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Back-end started on port 3333 🚀')
+});
